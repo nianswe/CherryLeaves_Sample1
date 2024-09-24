806fb629c7ebd4228623f866677e3d18249e4388
Niklas Andersson <nianswe@hotmail.com>
2024-09-24 14:06:04 +0000

    240924_1605

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -10,7 +10,7 @@ class MultiPage:
 
         st.set_page_config(
             page_title=self.app_name,
-            page_icon="🖥️")  # You may add an icon, to personalize your App
+            page_icon="leaves")  # You may add an icon, to personalize your App
         # check links below for additional icons reference
         # https://docs.streamlit.io/en/stable/api.html#streamlit.set_page_config
         # https://twemoji.maxcdn.com/2/test/preview.html
