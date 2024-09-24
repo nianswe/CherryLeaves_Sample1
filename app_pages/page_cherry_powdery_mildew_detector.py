ba8a8e972f752091c68483abe573629a667412dc
Niklas Andersson <nianswe@hotmail.com>
2024-09-24 12:40:52 +0000

    240924_1440

--- app_pages/page_cherry_powdery_mildew_detector.py
+++ app_pages/page_cherry_powdery_mildew_detector.py
@@ -27,22 +27,46 @@ def page_cherry_powdery_mildew_detector():
                                         type='jpg',accept_multiple_files=True)
    
     if images_buffer is not None:
-        df_report = pd.DataFrame([])
-        for image in images_buffer:
+#        df_report = pd.DataFrame([])
+#        for image in images_buffer:
+#
+#            img_pil = (Image.open(image))
+#            st.info(f"cherry leaves sample: **{image.name}**")
+#            img_array = np.array(img_pil)
+#            st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+#
+#            version = 'v1'
+#            resized_img = resize_input_image(img=img_pil, version=version)
+#            pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
+#            plot_predictions_probabilities(pred_proba, pred_class)
+#
+#            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
+#                                            ignore_index=True)
 
-            img_pil = (Image.open(image))
-            st.info(f"cherry leaves sample: **{image.name}**")
-            img_array = np.array(img_pil)
-            st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+        df_report = pd.DataFrame(columns=['Name', 'Result'])
 
-            version = 'v1'
-            resized_img = resize_input_image(img=img_pil, version=version)
-            pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
-            plot_predictions_probabilities(pred_proba, pred_class)
+        for image in tqdm(images_buffer, desc="Processing images"):
+            try:
+                img_pil = Image.open(image)
+                st.info(f"Cherry leaves sample: **{image.name}**")
+                
+                img_array = np.array(img_pil)
+                st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
 
-            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
-                                            ignore_index=True)
-                        
+                version = 'v1'
+                resized_img = resize_input_image(img=img_pil, version=version)
+                pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
+                plot_predictions_probabilities(pred_proba, pred_class)
+
+                df_report = df_report._append({'Name': image.name, 'Result': pred_class}, ignore_index=True)
+            
+            except Exception as e:
+                st.error(f"Error processing image {image.name}: {str(e)}")
+
+        st.dataframe(df_report)
+
+
+
         if not df_report.empty:
             st.success("Analysis Report")
             st.table(df_report)
