6fa278b1305681f53c249acc81f2ebea42944e32
Niklas Andersson <nianswe@hotmail.com>
2024-09-24 11:11:56 +0000

    240924_1313

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 # import plotly.express as px
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
