547ea143346d21ffa8e56ca313c978fe07297bbd
Niklas Andersson <nianswe@hotmail.com>
2024-09-24 11:22:05 +0000

    240924_1322

--- app_pages/page_cherry_powdery_mildew_detector.py
+++ app_pages/page_cherry_powdery_mildew_detector.py
@@ -40,8 +40,10 @@ def page_cherry_powdery_mildew_detector():
             pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
             plot_predictions_probabilities(pred_proba, pred_class)
 
-            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
-                                        ignore_index=True)
+            # df_report = df_report.append({"Name":image.name, 'Result': pred_class },
+            #                            ignore_index=True)
+
+            df_report = pd.DataFrame(columns=['Name', 'Result'])
         
         if not df_report.empty:
             st.success("Analysis Report")
